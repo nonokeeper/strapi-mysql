5773f7e6bb19c50b999b1543d23e8aaf9959670c
costearnaud <costearnaud@yahoo.fr>
2022-09-07 18:52:05 +0200

    env

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'localhost'),
-  port: env.int('PORT', 1338),
+  host: env('APP_HOST', 'localhost'),
+  port: env.int('NODE_PORT', 1338),
   app: {
     keys: env.array('APP_KEYS'),
   },
