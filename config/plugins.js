e0fce73a6627b948aea05575681b6cb4d45821e8
costearnaud <costearnaud@yahoo.fr>
2022-09-07 18:21:44 +0200

    env

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,20 @@ module.exports = ({ env }) => ({
           "https://api.vercel.com/v1/integrations/deploy/prj_EtObraYAb3MuYM04cwjrBJiW19Hj/2TZY2fqCVu",
         apiToken: "FPlPSSauMRHh5EueQpPQir67",
         appFilter: "strapi",
-        teamFilter: "strapi-team",
+        teamFilter: "",
         roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
       },
     },
+    'cloudflare-pages': {
+        enabled: true,
+        config: {
+            deployHook: "https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/6ee1187b-287e-4470-9251-ff072b219f0b",
+            instances: [
+                {
+                name: "production website",
+                hook_url: 'https://2fcf7a4b.strapi-mysql.pages.dev'
+                },
+            ]
+        }
+      }
   });
