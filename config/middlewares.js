ae5914cc39a302d13f1faf7598d80a0bf261e28f
costearnaud <costearnaud@yahoo.fr>
2022-10-02 18:27:16 +0200

    vercel deploy test

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,38 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://admin.bacv78.fr'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://admin.bacv78.fr'
+          ],
+          upgradeInsecureRequests: null,
+        }
+      }
+    }
+  },
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin : ['http://localhost:3000', 'http://localhost:1338', 'https://1b08-92-88-6-192.eu.ngrok.io', 'https://mariesophie-book.vercel.app']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
